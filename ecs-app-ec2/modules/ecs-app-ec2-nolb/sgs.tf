61dcc4d2c35650c190fa5e5ee0ccb43c696be6fb
David Třebický <davidtrebicky@gmail.com>
2019-06-04 13:46:52 +0200

    remove sg inbound rule from load balancer source

--- ecs-app-ec2/modules/ecs-app-ec2-nolb/sgs.tf
+++ ecs-app-ec2/modules/ecs-app-ec2-nolb/sgs.tf
@@ -3,13 +3,6 @@ resource "aws_security_group" "application" {
   vpc_id      = "${var.vpc_id}"
   description = "${local.name} security group"
 
-  ingress {
-    protocol        = "tcp"
-    from_port       = 1024
-    to_port         = 65535
-    security_groups = ["${data.aws_lb.alb.security_groups}"]
-  }
-
   egress {
     protocol    = -1
     from_port   = 0
