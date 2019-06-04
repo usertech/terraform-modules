b7466cb6677a4a98bd990d81b8f68d456b121f5d
David Třebický <davidtrebicky@gmail.com>
2019-06-04 13:38:28 +0200

    no lb app

---
+++ ecs-app-ec2/modules/ecs-app-ec2-nolb/service.tf
@@ -0,0 +1,21 @@
+data "aws_ecs_task_definition" "application" {
+  task_definition = "${aws_ecs_task_definition.application.family}"
+  depends_on      = ["aws_ecs_task_definition.application"]
+}
+
+resource "aws_ecs_service" "application" {
+  name    = "${var.name}"
+  cluster = "${data.aws_ecs_cluster.ecs.arn}"
+
+  task_definition                    = "${aws_ecs_task_definition.application.family}:${max("${aws_ecs_task_definition.application.revision}", "${data.aws_ecs_task_definition.application.revision}")}"
+  desired_count                      = "${var.min_capacity}"
+  launch_type                        = "EC2"
+  deployment_maximum_percent         = "${var.max_healthy}"
+  deployment_minimum_healthy_percent = "${var.min_healthy}"
+  health_check_grace_period_seconds  = "${var.healthcheck_grace}"
+  scheduling_strategy                = "${var.scheduling_strategy}"
+
+  depends_on = ["aws_iam_role.ecs_task_execution"]
+
+  tags = "${var.tags}"
+}
